34a3b8b3bb8d0289eb2fd1bef7eeb22f52ea00d5
ArieGoldkin <34339295+ArieGoldkin@users.noreply.github.com>
2025-07-21 10:56:03 +0300

    feat: add AppThemeToggle export to UI components index

---
+++ packages/app/src/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { Button } from "./button";
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+export { Input } from "./input";
+export { Label } from "./label";
+export { Checkbox } from "./checkbox";
+export { AppThemeToggle } from "./theme-toggle";
